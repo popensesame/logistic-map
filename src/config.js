50bbb9b11a9dd90b2c5ec248eb8660a194910aaa
Matthew Geiger <matthewg@ecobotapp.com>
2022-11-16 10:13:17 -0500

    More controls

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const config = {
   r0     : 0.5,
   r1     : 5,
-  rStep  : .0001,
-  xSize  : 30,
-  width: 1500,
+  rStep  : .001,
+  xSize  : 1000,
+  width: 1200,
   height: 800
 }
 
--- src/index.js
+++ src/index.js
@@ -22,69 +22,115 @@ const yScale = d3.scaleLinear()
 
 // Start
 
-const canvas = document.getElementById('canvas')
-canvas.width = config.width
-canvas.height = config.height
-
-var ctx = canvas.getContext('2d')
-ctx.fillStyle = 'rgb(0, 0, 0)'
-
-let graph
-
-function newGraph() {
-	return new LGraph(config.r0, config.r1, config.rStep, config.xSize)
-}
-
-function clear(ctx) {
-	ctx.clearRect(0, 0, width, height);
-}
-
-function draw() {
-  ctx.save();
-  ctx.clearRect(0, 0, width, height);
-  ctx.fill();
-  ctx.restore();
-  for (let slice of graph.slices) {
-    const data = slice.get()
-    for (let v of data) {
-      const x = xScale(parseFloat(slice.r))
-      const y = yScale(parseFloat(v))
-      ctx.fillRect(x, y, POINT_SIZE, POINT_SIZE)
-    }
-  }
-  graph.next()
-	if (animating) {
-		window.requestAnimationFrame(draw)
-	} else {
-		console.log('Stopping...')
+class LogMapCanvas {
+	constructor(conf) {
+		Object.assign(this, conf)
+		this.canvas = document.getElementById(conf.id || 'canvas')
+		this.canvas.width = conf.width
+		this.canvas.height = conf.height
+		this.ctx = this.canvas.getContext('2d')
+		this.ctx.fillStyle = 'rgb(0, 0, 0)'
+		this.graph = this.newGraph(conf.r0, conf.r1, conf.rStep, conf.xSize)
+		this.initControls(conf)
 	}
-  
+
+	newGraph(conf) {
+		conf = conf || {}
+		return new LGraph(
+			conf.r0 || this.r0,
+			conf.r1 || this.r1,
+			conf.rStep || this.rStep,
+			conf.xSize || this.xSize
+		)
+	}
+
+	resetGraph() {
+		this.animating = false
+		this.graph = this.newGraph()
+		this.clear(this.ctx)	
+	}
+
+	clear() {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+
+	resetCanvas(conf) {
+		this.clear()
+		this.canvas.width = conf.width || this.canvas.width
+		this.canvas.height = conf.height || this.canvas.height
+		this.graph = newGraph()
+	}
+
+	draw() {
+		this.ctx.save();
+		this.ctx.clearRect(0, 0, width, height);
+		this.ctx.fill();
+		this.ctx.restore();
+		for (let slice of this.graph.slices) {
+			const data = slice.get()
+			for (let v of data) {
+				const x = xScale(parseFloat(slice.r))
+				const y = yScale(parseFloat(v))
+				this.ctx.fillRect(x, y, POINT_SIZE, POINT_SIZE)
+			}
+		}
+		this.graph.next()
+		if (this.animating) {
+			window.requestAnimationFrame(this.draw.bind(this))
+		} else {
+			console.log('Stopping...')
+		}
+	}
+
+	initControls(conf) {
+		this.ctl = {
+			start: document.getElementById('start'),
+			stop: document.getElementById('stop'),
+			next: document.getElementById('next'),
+			clear: document.getElementById('clear'),
+			save: document.getElementById('save'),
+
+			r0: document.getElementById('r0'),
+			r1: document.getElementById('r1'),
+			rStep: document.getElementById('rStep'),
+			xSize: document.getElementById('xSize'),
+			width: document.getElementById('width'),
+			height: document.getElementById('height')
+		}
+
+		const keys = [ 'r0', 'r1', 'rStep', 'xSize', 'width', 'height' ]
+		keys.forEach(k => {
+			this.ctl[k].value = conf[k]
+			this.ctl[k].addEventListener('change', e => {
+				this[k] = parseInt(e.target.value)
+			})
+		})
+
+		this.ctl.start.addEventListener('click', e => {
+			this.animating = true
+			this.graph = this.graph || this.newGraph()
+			console.log('Starting...')
+			window.requestAnimationFrame(this.draw.bind(this));
+		})
+
+		this.ctl.stop.addEventListener('click', e => {
+			this.animating = false
+		})
+
+		this.ctl.next.addEventListener('click', e => {
+			window.requestAnimationFrame(this.draw);
+		})
+
+		this.ctl.clear.addEventListener('click', e => {
+			this.resetGraph()
+		})
+
+		this.ctl.save.addEventListener('click', e => {
+			this.animating = false
+			const url = canvas.toDataURL('image/jpeg')
+			window.open(url)
+		})
+	}
 }
 
-let animating = true
-
-document.getElementById('start')
-	.addEventListener('click', e => {
-		animating = true
-		graph = graph || newGraph()
-		console.log('Starting...')
-		window.requestAnimationFrame(draw);
-	})
-
-document.getElementById('stop')
-	.addEventListener('click', e => {
-		animating = false
-	})
-
-document.getElementById('next')
-	.addEventListener('click', e => {
-		window.requestAnimationFrame(draw);
-	})
-
-document.getElementById('clear')
-	.addEventListener('click', e => {
-		animating = false
-		graph = newGraph()
-		clear(ctx)
-	})
-
+const canvas = new LogMapCanvas(config)
